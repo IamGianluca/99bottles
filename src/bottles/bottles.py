1d80287cf71148d8b40def5a38bbc92a0ec169c7
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-07-02 14:51:46 -0400

    CH7: factory with self-registration

--- src/bottles/bottles.py
+++ src/bottles/bottles.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Bottle:
     def song(self) -> str:
         return self.verses(99, 0)
@@ -8,7 +11,7 @@ class Bottle:
         )
 
     def verse(self, number: int) -> str:
-        bn = BottleNumber.get(number)
+        bn = BottleNumberFactory.get(number)
         return (
             f"{bn.to_str().capitalize()} of beer on the wall, {bn.to_str()} of beer.\n"
             f"{bn.action()}, {bn.successor().to_str()} of beer on the wall.\n"
@@ -19,17 +22,13 @@ class BottleNumber:
     def __init__(self, number: int) -> None:
         self.number = number
 
+    @classmethod
+    def register(self, factory):
+        factory.add(self)
+
     @staticmethod
-    def get(number: int):
-        """BottleNumber factory."""
-        if number == 0:
-            return BottleNumber0(number)
-        elif number == 1:
-            return BottleNumber1(number)
-        elif number == 6:
-            return BottleNumber6(number)
-        else:
-            return BottleNumber(number)
+    def can_handle(number: int) -> bool:
+        return True
 
     def to_str(self) -> str:
         return " ".join([self.quantity(), self.container()])
@@ -47,10 +46,14 @@ class BottleNumber:
         return f"Take {self.pronoun()} down and pass it around"
 
     def successor(self) -> int:
-        return self.get(self.number - 1)
+        return BottleNumberFactory.get(self.number - 1)
 
 
 class BottleNumber0(BottleNumber):
+    @staticmethod
+    def can_handle(number: int) -> bool:
+        return number == 0
+
     def quantity(self) -> str:
         return "no more"
 
@@ -58,10 +61,14 @@ class BottleNumber0(BottleNumber):
         return "Go to the store and buy some more"
 
     def successor(self) -> int:
-        return self.get(99)
+        return BottleNumberFactory.get(99)
 
 
 class BottleNumber1(BottleNumber):
+    @staticmethod
+    def can_handle(number: int) -> bool:
+        return number == 1
+
     def container(self) -> str:
         return "bottle"
 
@@ -70,8 +77,35 @@ class BottleNumber1(BottleNumber):
 
 
 class BottleNumber6(BottleNumber):
+    @staticmethod
+    def can_handle(number: int) -> bool:
+        return number == 6
+
     def quantity(self) -> str:
         return "1"
 
     def container(self) -> str:
         return "six-pack"
+
+
+class BottleNumberFactory:
+    registry: List = []
+
+    @classmethod
+    def add(cls, item: BottleNumber):
+        cls.registry.append(item)
+
+    @classmethod
+    def get(cls, number: int):
+        """BottleNumber factory."""
+        bottle_number_class = [
+            bn for bn in cls.registry if bn.can_handle(number)
+        ]
+        return bottle_number_class[0](number)
+
+
+# register all BottleNumbers
+for cls in BottleNumber.__subclasses__():
+    print(cls)
+    cls.register(BottleNumberFactory)
+BottleNumber.register(BottleNumberFactory)
